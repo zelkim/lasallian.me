57985873e838380e62c6a9659fe3d1eeb61d58b3
ejsadiarin <kyube.ejs@proton.me>
2025-02-02 00:55:43 +0800

    refactor: update Post model to add badge support

--- models/Post.js
+++ models/Post.js
@@ -9,7 +9,8 @@ const postSchema = new mongoose.Schema({
         updated_at: { type: Date, required: true, default: Date.now }
     },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'comments' }
+    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'comments' },
+    badge: { type: mongoose.Schema.Types.ObjectId, ref: 'badges' }
 })
 
 export default mongoose.model("posts", postSchema)
