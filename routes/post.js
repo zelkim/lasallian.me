fbaf45df2d7cbce7faec4ecb8d4c7985a8fa4a5c
ejsadiarin <ejs@ejsadiarin.com>
2025-03-04 20:13:47 +0800

    feat: add get all posts

--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,17 @@
 import express from 'express'
-import { GetNormalPostById, GetAllNormalPostByAuthor, CreateNormalPost, UpdatePost, DeletePost } from '../services/post.js'
+import { GetAllPosts, GetNormalPostById, GetAllNormalPostByAuthor, CreateNormalPost, UpdatePost, DeletePost } from '../services/post.js'
 import { validateSession } from '../services/session.js'
 
 const router = express.Router()
 
-// user-specific posts
+router.get("/all", GetAllPosts) // /post
+
+// normal posts
 router.get("/normal/:id", validateSession, GetNormalPostById) // /post/normal/:id (for getting specific post) 
 router.get("/normal", validateSession, GetAllNormalPostByAuthor) // gets all normal posts of authenticated user
-
 router.post("/normal", validateSession, CreateNormalPost) // creates normal post
+
+// normal posts
 
 router.put("/normal/:id", validateSession, UpdatePost) // updates post
 
