af12383a7d518cde6f3d2dfe62ec1b546d56dd6e
ejsadiarin <ejs@ejsadiarin.com>
2025-03-05 16:58:11 +0800

    fix: refs and add new refs

--- models/OrgMembers.js
+++ models/OrgMembers.js
@@ -1,29 +1,29 @@
 import mongoose from 'mongoose'
 
 const orgMemberSchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-  joindate: { type: Date, required: true },
-  position: {
-    type: String,
-    enum:
-      [
-        'PRES',
-        'EVP',
-        'VP',
-        'AVP',
-        'CT',
-        'JO',
-        'MEM'
-      ],
-    default: 'MEM',
-    required: true
-  },
-  media: [{ type: String }],
-  meta: {
-    created_at: { type: Date, required: true, default: Date.now },
-    updated_at: { type: Date, required: true, default: Date.now }
-  },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user_info', required: true },
+    org: { type: mongoose.Schema.Types.ObjectId, ref: 'org_info', required: true },
+    joindate: { type: Date, required: true },
+    position: {
+        type: String,
+        enum:
+            [
+                'PRES',
+                'EVP',
+                'VP',
+                'AVP',
+                'CT',
+                'JO',
+                'MEM'
+            ],
+        default: 'MEM',
+        required: true
+    },
+    media: [{ type: String }],
+    meta: {
+        created_at: { type: Date, required: true, default: Date.now },
+        updated_at: { type: Date, required: true, default: Date.now }
+    },
 })
 
 export default mongoose.model("org_members", orgMemberSchema)
