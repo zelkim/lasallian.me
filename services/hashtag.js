124838fc4e25da9563d22522a6cd516340d45895
zelkim <dev86.sean@gmail.com>
2025-03-07 02:18:26 +0800

    feat(hashtag): created hashtag parser for posts and searchHastag

---
+++ services/hashtag.js
@@ -0,0 +1,30 @@
+export const parseHashtags = (body) => {
+  const hashtags = body.match(/#[a-zA-Z0-9_]+/g);
+  return matches ? matches : [];
+};
+
+const searchHashtags = async (query) => {
+  const regex = new RegExp(`^${query}`, "i");
+
+  const result = await Post.aggregate([
+    { $unwind: "$hashtags" },
+    {
+      $match: { "hashtags.tag": regex },
+    },
+    {
+      $group: {
+        _id: "$hashtags.tag",
+        posts: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        tag: "$_id",
+        posts: 1,
+      },
+    },
+  ]);
+
+  return result;
+};
