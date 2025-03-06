e062799c4139d34c50a143ac105493e57ad3f2b8
zelkim <dev86.sean@gmail.com>
2025-03-07 02:03:24 +0800

    feat(hashtag)!: updated post model

--- models/Post.js
+++ models/Post.js
@@ -1,38 +1,53 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 export const POST_TYPES = {
-    NORMAL: "normal",
-    PROJECT: "project",
-    EVENT: "event",
-}
+  NORMAL: "normal",
+  PROJECT: "project",
+  EVENT: "event",
+};
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: false },
-    content: { type: Object, required: true },
-    media: [{ type: String }],
-    type: {
+  title: { type: String, required: false },
+  content: { type: Object, required: true },
+  media: [{ type: String }],
+  type: {
+    type: String,
+    required: true,
+    enum: Object.values(POST_TYPES),
+    default: POST_TYPES.NORMAL,
+  },
+  visibility: {
+    type: String,
+    enum: ["public", "organization", "private"],
+    default: "public",
+  },
+  meta: {
+    created_at: { type: Date, required: true, default: Date.now },
+    updated_at: { type: Date, required: true, default: Date.now },
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user_info",
+    required: true,
+  },
+  comments: { type: mongoose.Schema.Types.ObjectId, ref: "comments" },
+  hashtags: [
+    {
+      tag: {
         type: String,
+        match: /^#[a-zA-Z0-9_]+$/,
         required: true,
-        enum: Object.values(POST_TYPES),
-        default: POST_TYPES.NORMAL
+      },
     },
-    visibility: {
-        type: String,
-        enum: ['public', 'organization', 'private'],
-        default: 'public'
+  ],
+  organization: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "org_info",
+    required: function () {
+      // require organization ref only if the post type is "event"
+      return this.type === POST_TYPES.EVENT;
     },
-    meta: {
-        created_at: { type: Date, required: true, default: Date.now },
-        updated_at: { type: Date, required: true, default: Date.now }
-    },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user_info', required: true },
-    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'comments' },
-    organization: {
-        type: mongoose.Schema.Types.ObjectId, ref: 'org_info', required: function () {
-            // require organization ref only if the post type is "event"
-            return this.type === POST_TYPES.EVENT;
-        }
-    }
-})
+  },
+});
 
-export default mongoose.model("posts", postSchema)
+export default mongoose.model("posts", postSchema);
