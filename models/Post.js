bf69c3ea6e956a71ff2fecab5f1a55d508f4ff0d
zelkim <dev86.sean@gmail.com>
2025-03-07 03:29:29 +0800

    fix(hashtag)!: rebased (again) to ACTUAL master

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 export const POST_TYPES = {
-    NORMAL: "normal",
-    PROJECT: "project",
-    EVENT: "event",
-}
+    NORMAL: 'normal',
+    PROJECT: 'project',
+    EVENT: 'event',
+};
 
 const postSchema = new mongoose.Schema({
     title: { type: String, required: false },
@@ -14,25 +14,43 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true,
         enum: Object.values(POST_TYPES),
-        default: POST_TYPES.NORMAL
+        default: POST_TYPES.NORMAL,
     },
     visibility: {
         type: String,
         enum: ['public', 'organization', 'private'],
-        default: 'public'
+        default: 'public',
     },
     meta: {
         created_at: { type: Date, required: true, default: Date.now },
-        updated_at: { type: Date, required: true, default: Date.now }
+        updated_at: { type: Date, required: true, default: Date.now },
     },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user_info', required: true },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user_info',
+        required: true,
+    },
     comments: { type: mongoose.Schema.Types.ObjectId, ref: 'comments' },
+    hashtags: [
+        {
+            tag: {
+                type: String,
+                match: /^#[a-zA-Z0-9_]+$/,
+                required: true,
+            },
+        },
+    ],
     organization: {
-        type: mongoose.Schema.Types.ObjectId, ref: 'org_info', required: function () {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'org_info',
+        required: function () {
             // require organization ref only if the post type is "event"
-            return this.type === POST_TYPES.EVENT || this.visibility === "organization";
-        }
-    }
-})
+            return (
+                this.type === POST_TYPES.EVENT ||
+                this.visibility === 'organization'
+            );
+        },
+    },
+});
 
-export default mongoose.model("posts", postSchema)
+export default mongoose.model('posts', postSchema);
