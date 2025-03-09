7d92f8a260f80729def2fb71ad93e996c90f3fc2
ejsadiarin <ejs@ejsadiarin.com>
2025-03-09 20:10:56 +0800

    fix: author refs now points to user_info

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,14 @@
 import { Schema, model } from 'mongoose';
 
 const commentSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true },
-  content: { type: String, required: true },
-  post: { type: Schema.Types.ObjectId, required: true, ref: 'posts' },
-  reactions: [{ type: Schema.Types.ObjectId, ref: 'comment_reactions' }],
-  meta: {
-    created_at: { type: Date, required: true, default: Date.now },
-    updated_at: { type: Date, required: true, default: Date.now },
-  },
+    author: { type: Schema.Types.ObjectId, required: true, ref: 'user_info' },
+    content: { type: String, required: true },
+    post: { type: Schema.Types.ObjectId, required: true, ref: 'posts' },
+    reactions: [{ type: Schema.Types.ObjectId, ref: 'comment_reactions' }],
+    meta: {
+        created_at: { type: Date, required: true, default: Date.now },
+        updated_at: { type: Date, required: true, default: Date.now },
+    },
 });
 
 export default model('comments', commentSchema);
