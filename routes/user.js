d07dd5f0395a6318083c587b1ca3ecd235ad0236
ejsadiarin <ejs@ejsadiarin.com>
2025-03-09 17:58:54 +0800

    feat(edit-profile): add edit profile to edit user profile details including credentialss

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { validateSession } from '../services/session.js'
-import { createInfo, createCredentials, authenticate, getUserById, getSessionUser, getUserByEmail } from '../services/user.js'
+import { createInfo, updateInfo, createCredentials, authenticate, getUserById, getSessionUser, getUserByEmail } from '../services/user.js'
 
 const router = express.Router()
 
@@ -9,9 +9,11 @@ router.post('/register', createCredentials)
 router.post('/setup', validateSession, createInfo)
 router.post('/login', authenticate)
 
+
 // - CRUD routes
 router.get('/', validateSession, getSessionUser)
 router.get('/:id', validateSession, getUserById)
 router.post('/get-by-email', validateSession, getUserByEmail)
+router.put('/', validateSession, updateInfo)
 
 export default router
